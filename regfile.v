1e045578201db5125946141b59ac77c1c8f93310
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2024-07-12 08:24:04 -0500

    Update regfile.v
    cambiado para que funcione con vectores

--- regfile.v
+++ regfile.v
@@ -6,8 +6,12 @@ module regfile (
 	wa3,
 	wd3,
 	r15,
+	vector_op,
 	rd1,
-	rd2
+	rd2,
+	VsrcA,
+	VsrcB,
+	index
 );
 	input wire clk;
 	input wire we3;
@@ -16,12 +20,35 @@ module regfile (
 	input wire [3:0] wa3;
 	input wire [31:0] wd3;
 	input wire [31:0] r15;
+	input wire [3:0] index;
+	input wire vector_op; // Señal para operaciones vectoriales
 	output wire [31:0] rd1;
 	output wire [31:0] rd2;
-	reg [31:0] rf [14:0];
+	output wire [31:0] VsrcA [0:4];
+	output wire [31:0] VsrcB [0:4];
+	reg [31:0] rf [14:0]; // Arreglo de registros
+
 	always @(posedge clk)
-		if (we3)
-			rf[wa3] <= wd3;
-	assign rd1 = (ra1 == 4'b1111 ? r15 : rf[ra1]);
-	assign rd2 = (ra2 == 4'b1111 ? r15 : rf[ra2]);
+		if (we3) begin
+			rf[wa3] <= wd3; // Escritura en el registro indicado por wa3
+		end
+
+	assign rd1 = (vector_op == 1'b0) ? (ra1 == 4'b1111 ? r15 : rf[ra1]) : 'bx;
+	assign rd2 = (vector_op == 1'b0) ? (ra2 == 4'b1111 ? r15 : rf[ra2]) : 'bx;
+
+	// Asignación de VsrcA y VsrcB para operaciones vectoriales
+	integer i;
+	always @(*) begin
+		if (vector_op) begin
+			for (i = 0; i < index; i = i + 1) begin
+				VsrcA[i] = rf[ra1 + i];
+				VsrcB[i] = rf[ra2 + i];
+			end
+		end else begin
+			for (i = 0; i < index; i = i + 1) begin
+				VsrcA[i] = 32'bx;
+				VsrcB[i] = 32'bx;
+			end
+		end
+	end
 endmodule
