5fc7d43f6f20ca8257b34a6fb2f175a3d7014d7a
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2024-07-15 13:49:40 -0500

    Update vregfile.v

--- vregfile.v
+++ vregfile.v
@@ -1,54 +1,45 @@
 module vregfile (
-	clk,
-	we3,
-	ra1,
-	ra2,
-	wa3,
-	wd3,
-	r15,
-	vector_op,
-	rd1,
-	rd2,
-	VsrcA,
-	VsrcB,
-	index
+    input wire clk,
+    input wire we3,
+    input wire vector_op,
+    input wire [3:0] ra1,
+    input wire [3:0] ra2,
+    input wire [3:0] wa3,
+    input wire [31:0] wd1,
+    input wire [31:0] wd2,
+    input wire [31:0] wd3,
+    input wire [31:32] wd4,
+    input wire [31:32] wd5,
+    output wire [31:0] rd1,
+    output wire [31:32] rd2,
+    output wire [31:32] rd3,
+    output wire [31:32] rd4,
+    output wire [31:32] rd5,
+    output wire [31:32] rd6,
+    output wire [31:32] rd7,
+    output wire [31:32] rd8,
+    output wire [31:32] rd9,
+    output wire [31:32] rd10
 );
-	input wire clk;
-	input wire we3;
-	input wire [3:0] ra1;
-	input wire [3:0] ra2;
-	input wire [3:0] wa3;
-	input wire [31:0] wd3;
-	input wire [31:0] r15;
-	input wire [3:0] index;
-	input wire vector_op; // Señal para operaciones vectoriales
-	output wire [31:0] rd1;
-	output wire [31:0] rd2;
-	output wire [31:0] VsrcA [0:4];
-	output wire [31:0] VsrcB [0:4];
-	reg [31:0] rf [14:0]; // Arreglo de registros
+    reg [31:0] rf [0:14][0:4];
+    always @(posedge clk) begin
+        if (we3 && vector_op) begin
+            rf[wa3][0] <= wd1;
+            rf[wa3][1] <= wd2;
+            rf[wa3][2] <= wd3;
+            rf[wa3][3] <= wd4;
+            rf[wa3][4] <= wd5;
+        end
+    end
+    assign rd1 = (vector_op) ? rf[ra1][0] : 32'bx;
+    assign rd2 = (vector_op) ? rf[ra1][1] : 32'bx;
+    assign rd3 = (vector_op) ? rf[ra1][2] : 32'bx;
+    assign rd4 = (vector_op) ? rf[ra1][3] : 32'bx;
+    assign rd5 = (vector_op) ? rf[ra1][4] : 32'bx;
+    assign rd6 = (vector_op) ? rf[ra2][0] : 32'bx;
+    assign rd7 = (vector_op) ? rf[ra2][1] : 32'bx;
+    assign rd8 = (vector_op) ? rf[ra2][2] : 32'bx;
+    assign rd9 = (vector_op) ? rf[ra2][3] : 32'bx;
+    assign rd10 = (vector_op) ? rf[ra2][4] : 32'bx;
 
-	always @(posedge clk)
-		if (we3) begin
-			rf[wa3] <= wd3; // Escritura en el registro indicado por wa3
-		end
-
-	assign rd1 = (vector_op == 1'b0) ? (ra1 == 4'b1111 ? r15 : rf[ra1]) : 'bx;
-	assign rd2 = (vector_op == 1'b0) ? (ra2 == 4'b1111 ? r15 : rf[ra2]) : 'bx;
-
-	// Asignación de VsrcA y VsrcB para operaciones vectoriales
-	integer i;
-	always @(*) begin
-		if (vector_op) begin
-			for (i = 0; i < index; i = i + 1) begin
-				VsrcA[i] = rf[ra1 + i];
-				VsrcB[i] = rf[ra2 + i];
-			end
-		end else begin
-			for (i = 0; i < index; i = i + 1) begin
-				VsrcA[i] = 32'bx;
-				VsrcB[i] = 32'bx;
-			end
-		end
-	end
 endmodule
