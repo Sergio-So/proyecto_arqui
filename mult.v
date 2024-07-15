1b98b6a10a319991f3e7d6f4eb7e3cda002ad96e
MarielUTEC <91162593+MarielUTEC@users.noreply.github.com>
2024-07-15 17:47:41 -0500

    Update mult.v

--- mult.v
+++ mult.v
@@ -1,11 +1,12 @@
-module mult (
+module mult(
     input wire [31:0] a,
     input wire [31:0] b,
     output wire [31:0] result,
-	output wire [31:0] overflow
+    output wire [31:0] overflow
 );
-	wire [64:0] rs;
+    wire [63:0] rs;
     assign rs = a * b; // Multiplicación
-	assign result = rs[31:0];
-	assign overflow = rs[63:32];
+    assign result = rs[31:0];
+    assign overflow = rs[63:32];
 endmodule
+
