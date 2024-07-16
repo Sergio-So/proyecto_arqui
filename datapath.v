d1e54a014e92ad0879f812c2c590fb87ceb18081
MarielUTEC <91162593+MarielUTEC@users.noreply.github.com>
2024-07-16 14:58:07 -0500

    ultimito

--- datapath.v
+++ datapath.v
@@ -49,7 +49,6 @@ module datapath (
 	wire [2:0] vector_size;
 	wire [31:0] VALUResultA,VALUResultB, VALUResultC, VALUResultD, VALUResultE;
 	wire [31:0] rd1, rd2, rd3, rd4, rd5, rd6, rd7, rd8, rd9, rd10;
-	wire [3:0] VALUFlags;
 	wire [31:0] rslt;
 	wire [3:0] ab;
 	wire [3:0] cd;
@@ -180,7 +179,6 @@ module datapath (
 		.VALUResultC(VALUResultC),
 		.VALUResultD(VALUResultD),
 		.VALUResultE(VALUResultE),
-		.ALUFlags(VALUFlags),
 		.Op(a),
 		.vector_op(vector_op)
 	);
